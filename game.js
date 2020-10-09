e122ebb163219b51dfc6bee5fe999e351ac20fc7
JosephAbuanor <joseph.abuanor@amalitech.org>
2020-10-09 10:08:00 +0000

    Modified Game and Style files

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const scoreText = document.getElementById('score');
 const progressText = document.getElementById('progressText');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -9,36 +11,41 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: 'Inside which HTML element do we put the JavaScript??',
-        choice1: '<script>',
-        choice2: '<javascript>',
-        choice3: '<js>',
-        choice4: '<scripting>',
-        answer: 1,
-    },
-    {
-        question:
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3,
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4,
-    },
-];
+let questions = [];
+fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple') //link to questions from Open Trivia
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+        game.classList.remove('hidden');
+        loader.classList.add('hidden');
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 const correct_bonus = 10;
-const max_questions = 3;
+const max_questions = 5;
 
 startGame = ()=> {
   questionCounter = 0;
@@ -46,25 +53,26 @@ startGame = ()=> {
   availableQuestions = [...questions];
   getNewQuestion();
 
-}
+};
 
 getNewQuestion = () =>{
 
 if (availableQuestions.length == 0||questionCounter >=  max_questions){
   localStorage.setItem("mostRecentScore", score);
   return window.location.assign("end.html");
-
 }
 
 questionCounter++;
 progressText.innerText = "Question" +questionCounter + "/" + max_questions;
-progressBarFulli = (questionCounter/max_questions)*100;
-progressBarFull.style.width = progressBarFulli+ "%";
+progressBarFullval = (questionCounter/max_questions)*100;
+progressBarFull.style.width = progressBarFullval+ "%";
 
 
 const questionIndex = Math.floor(Math.random() * availableQuestions.length);
 currentQuestion = availableQuestions[questionIndex];
-question.innerText = currentQuestion.question ;
+currentQuestionVar = currentQuestion.question;
+newCurrentQuestion = currentQuestionVar.replace(/&quot;/g, '\"') //replace the "&quot;" that appears in the question with actual quotes.
+question.innerText = newCurrentQuestion ;
 
 choices.forEach(choice => {
   const number = choice.dataset["number"];
@@ -106,6 +114,3 @@ function incrementScore(num) {
 
   })
 });
-
-
-startGame();
